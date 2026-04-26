b70477efceb496be5e461395e3f1a114c70eeea5
uuganaauuganbayar <uuganbayar.dev@gmail.com>
2026-04-26 23:15:07 +0800

    chore: initial site build

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,9 @@ const nextConfig = {
     NEXT_PUBLIC_ERXES_APP_TOKEN: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjbGllbnRQb3J0YWxJZCI6IlZSSUwyMXk3UWh6UnprSzhuREVnTSIsImlhdCI6MTc3NzIxNDgyMn0.SnhaksTCrmc5dpum0X7IVRKtu5INLM18KAhj18u7WUw",
     NEXT_PUBLIC_ERXES_CMS_ID: "69ee261cd5623e433fd4a8fa",
   },
+  turbopack: {
+    root: ".",
+  },
 };
 
 export default nextConfig;
